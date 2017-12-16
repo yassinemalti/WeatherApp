02134c3d0d5b2fdce0718749310e0ccbc3417dfc
Yassine MALTI <yassinemalti@gmail.com>
2017-12-16 13:30:32 +0100

    Initial commit

---
+++ app/build/generated/source/buildConfig/debug/karthik/WeatherApp/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package karthik.WeatherApp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "karthik.WeatherApp";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.2";
+}
---
+++ app/src/main/java/karthik/WeatherApp/Config.java
@@ -0,0 +1,5 @@
+package karthik.WeatherApp;
+
+final class Config {
+	static final String APIKEY = "9d0edc7f9ca58dc2";
+}
